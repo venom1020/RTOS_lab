da37af69cb4907969ab0e5dd0a282205231b002a
venom1020 <aneeshachar10@gmail.com>
2022-10-01 06:55:03 +0530

    OCT1

--- 23sep_assignment/ex9/ex9.c
+++ 23sep_assignment/ex9/ex9.c
@@ -18,8 +18,8 @@ printf("Enter section:");
 scanf("%s",section);
 printf("Enter marks in five subject\n");
 for(int i=0;i<5;i++) scanf("%d",&marks[i]);
-fprintf(a,"# %s | ## %s | ###",name,section);
-for(int i=0;i<5;i++) fprintf(a," %d |",marks[i]);
+fprintf(a,"# %s\t | ## %s \t| ###",name,section);
+for(int i=0;i<5;i++) fprintf(a," %d ",marks[i]);
 fprintf(a,"\n");	
 
 }
