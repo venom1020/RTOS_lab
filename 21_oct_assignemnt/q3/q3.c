211b280236cd286ca8c719f55b8fe88b71087e34
venom1020 <aneeshachar10@gmail.com>
2022-11-22 06:47:27 +0530

    22 nov

--- 21_oct_assignemnt/q3/q3.c
+++ 21_oct_assignemnt/q3/q3.c
@@ -1,7 +1,47 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
 int main()
-{ 	
+{   FILE *a,*b;
+	int i=0,j=0,flag=0,l1=0,l2=0;
+	a=fopen("file1.txt","r");
+	b=fopen("file2.txt","r");
+	char lineF1[1000],lineF2[1000],c;
+	while(!feof(a) && !feof(b))
+	{ i=0; 
+	  c='\0';
+	  while(c!='.' && !feof(a)) 
+	  	{ 	c=fgetc(a); 
+	  	    lineF1[i++]=c; 	 
+	  	}
+	  lineF1[i]='\0';	
+	    j=0;
+	  c='\0';
+	  while(c!='.' && !feof(b)) 
+	  	{ c=fgetc(b);
+	  	  lineF2[j++]=c;
+	   	}
+	  lineF2[j]='\0';
+	  l1++;
+	  l2++;
+	  if(strcmp(lineF1,lineF2)!=0) 
+	  { flag=1;
+	  	break;
+
+	  }
+	}
+
+	if(flag==1) {
+		printf("\nChanges detected:\n");
+		printf("\nLine of file 1 (Line no:%d): %s",l1,lineF1);
+		printf("\n\nLine of file 2 (Line no:%d): %s",l2,lineF2);
+		fclose(a);
+		fclose(b);
+		return 0;
+	}
+	printf("\nNo changes detected!!!!");
+	fclose(a);
+	fclose(b);
 	return 0;
 }
--- 21_oct_assignemnt/q4/q4.c
+++ 21_oct_assignemnt/q4/q4.c
@@ -4,32 +4,40 @@
 
 FILE *output;
 
-void printPage(FILE *a);
+void printPage(char *a);
 int main()
 {	int n;
 	char fname[10];
 	printf("How many files to read:");
 	scanf("%d",&n);
-	FILE *a[n];
 	output=fopen("output.txt","w");
 	for(int i=0;i<n;i++){
 		sprintf(fname,"file%d.txt",i+1);
-		a[i]=fopen(fname,"r");
 		fprintf(output,"\n----------------------------\nTitle: File %d\n",i+1);
-	  	printPage(a[i]);
-	  	fprintf(output,"Page:%d",i+1);
+	  	printPage(fname);
+	  	fprintf(output,"\n\nPage:%d",i+1);
 	}
 
 	fclose(output);
 	return 0;
 }
 
-void printPage(FILE *a)
+void printPage(char *fname)
 { char temp[20];
+	FILE *a;
+	a=fopen(fname,"r");
+	if(a==NULL) 
+		{   printf("\n%s is Empty",fname);
+			return;
+		}
+	else
+	{	
 	while(!feof(a))
-	{ fscanf(a,"%s",temp);
-	  printf("%s",temp);
-	  fprintf(output,"%s",temp);
+	{
+	  fscanf(a,"%s",temp);
+	  fprintf(output,"%s ",temp);
 	}
- fclose(a);
+	}
+ 	fclose(a);
+ 	
 }
